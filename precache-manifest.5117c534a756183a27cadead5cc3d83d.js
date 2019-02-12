96f7e128ab5e718cc93f7eff0717af4a8d1c6642
akshanshverma <akshansh.verma@outlook.com>
2019-02-12 12:44:51 +0530

    Updates

---
+++ precache-manifest.5117c534a756183a27cadead5cc3d83d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1267ddf9d243b0f22902",
+    "url": "/static/css/main.0403c331.chunk.css"
+  },
+  {
+    "revision": "1267ddf9d243b0f22902",
+    "url": "/static/js/main.1267ddf9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6310a5c9899ad1b1e62c0a3803761019",
+    "url": "/index.html"
+  }
+];
